d095eb1efe952797fb4f21eb15b2a9bc173e7e2a
Michael Granados <contato@dgmike.com.br>
2024-08-13 14:31:32 -0300

    feat: added root route
    Signed-off-by: Michael Granados <contato@dgmike.com.br>

--- app/controllers.js
+++ app/controllers.js
@@ -1,6 +1,21 @@
-const root = ({ res }) => res.json({})
+const { Resource, Link } = require('hal')
 
-const health = ({ res }) => res.json({ ok: true })
+const links = {
+  root: '/',
+  health: '/health',
+}
+
+const root = ({ res }) => {
+  const response = new Resource({}, links.root)
+  response.link(new Link('health', links.health))
+  res.json(response)
+}
+
+const health = ({ res }) => {
+  const response = new Resource({ ok: true }, links.health)
+  response.link(new Link('root', links.root))
+  res.json(response)
+}
 
 module.exports = {
   root,
