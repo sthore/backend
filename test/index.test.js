437540cd041abc51848d7d1b40a95a87e11074a2
Michael Granados <contato@dgmike.com.br>
2024-08-13 15:51:54 -0300

    feat: added hal json content type header
    Signed-off-by: Michael Granados <contato@dgmike.com.br>

--- test/index.test.js
+++ test/index.test.js
@@ -3,13 +3,13 @@ const server = require('supertest')
 const app = require('../app')
 
 const CONTENT_TYPE = 'content-type'
-const MIME_JSON_UTF8 = 'application/hal+json; charset=utf-8'
+const MIME_HAL_JSON_UTF8 = 'application/hal+json; charset=utf-8'
 
 describe('app', () => {
   it('must resolve root', () => server(app)
     .get('/')
     .expect(200)
-    .expect(CONTENT_TYPE, MIME_JSON_UTF8)
+    .expect(CONTENT_TYPE, MIME_HAL_JSON_UTF8)
     .expect({
       _links: {
         self: { href: '/' },
@@ -20,7 +20,7 @@ describe('app', () => {
   it('must start a server with health', () => server(app)
     .get('/health')
     .expect(200)
-    .expect(CONTENT_TYPE, MIME_JSON_UTF8)
+    .expect(CONTENT_TYPE, MIME_HAL_JSON_UTF8)
     .expect({
       ok: true,
       _links: {
