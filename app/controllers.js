cd91cefa81abe45671de00f3bd1794ba5e4548a3
Michael Granados <contato@dgmike.com.br>
2024-08-15 11:22:19 -0300

    feat: check product id format
    Signed-off-by: Michael Granados <contato@dgmike.com.br>

--- app/controllers.js
+++ app/controllers.js
@@ -35,7 +35,16 @@ const products = {
     res.header('content-type', HAL_JSON).json(response)
   },
   fetch: async ({ app, req: { params }, res }) => {
-    const product = await app.get('db').model('products').findOne(params.id)
+    const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+    if (!UUID_REGEX.test(params.id)) {
+      res.status(404).json({})
+      return
+    }
+    const product = await app.get('db').model('products').findByPk(params.id)
+    if (!product) {
+      res.status(404).json({})
+      return
+    }
     const response = new Resource(product.toJSON(), links.products.item(product.id))
     response.link('products', links.products.find)
     res.header('content-type', HAL_JSON).json(response)
