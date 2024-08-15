0b795b9a433f2278e9b5f7dcfe8ebf071a96dcf6
Michael Granados <contato@dgmike.com.br>
2024-08-15 11:22:19 -0300

    feat: check product id format
    Signed-off-by: Michael Granados <contato@dgmike.com.br>

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,7 @@ const mount = (app) => {
   app.get('/', controllers.root)
   app.get('/health', controllers.health)
   app.get('/api/v1/products', controllers.products.find)
+  app.get('/api/v1/products/:id', controllers.products.fetch)
 }
 
 module.exports = {
