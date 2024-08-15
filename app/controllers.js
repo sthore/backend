a0bdd78b0f9e29ed771f5d5813e8dc0d60f6c131
Michael Granados <contato@dgmike.com.br>
2024-08-15 09:43:05 -0300

    feat: route for list produts
    Signed-off-by: Michael Granados <contato@dgmike.com.br>

--- app/controllers.js
+++ app/controllers.js
@@ -5,6 +5,10 @@ const HAL_JSON = 'application/hal+json; charset=utf-8'
 const links = {
   root: '/',
   health: '/health',
+  products: {
+    find: '/api/v1/products',
+    item: (id) => `/api/v1/products/${id}`,
+  },
 }
 
 const root = ({ res }) => {
@@ -19,7 +23,20 @@ const health = ({ res }) => {
   res.header('content-type', HAL_JSON).json(response)
 }
 
+const products = {
+  find: async ({ req, res }) => {
+    const toResource = (item) =>
+      new Resource(item.toJSON(), links.products.item(item.id))
+    const products = await req.app.get('db').model('products').findAll()
+    const response = new Resource({}, links.products.find)
+    response.embed('products', products.map(toResource))
+    response.link('root', links.root)
+    res.header('content-type', HAL_JSON).json(response)
+  },
+}
+
 module.exports = {
   root,
   health,
+  products,
 }
--- app/database.js
+++ app/database.js
@@ -4,7 +4,22 @@ const setup = async (options) => {
   const sequelize = new Sequelize(options)
 
   sequelize.define('products', {
-    sku: Sequelize.DataTypes.STRING,
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    sku: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+    description: Sequelize.DataTypes.TEXT,
   })
 
   return sequelize
