dff802b651cd0376c4ea67cb687bf4c1ec8ff255
Michael Granados <contato@dgmike.com.br>
2024-08-15 09:45:17 -0300

    chore: format file
    Signed-off-by: Michael Granados <contato@dgmike.com.br>

--- test/config.js
+++ test/config.js
@@ -3,20 +3,19 @@ const { newDb } = require('pg-mem')
 const mem = newDb()
 
 module.exports = {
-    app: {
-      database: {
-        dialect: 'postgres',
-        dialectModule: mem.adapters.createPg(),
-        timezone: 'America/Sao_Paulo',
-        define: {
-          timestamps: true,
-          paranoid: true,
-        },
-        benchmark: true,
-        sync: {
-          force: true,
-        },
+  app: {
+    database: {
+      dialect: 'postgres',
+      dialectModule: mem.adapters.createPg(),
+      timezone: 'America/Sao_Paulo',
+      define: {
+        timestamps: true,
+        paranoid: true,
+      },
+      benchmark: true,
+      sync: {
+        force: true,
       },
     },
-  }
-  
+  },
+}
